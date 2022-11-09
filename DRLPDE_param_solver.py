ea379f09eaf791dc4a8ffc9dd2c04ac2b86270e3
Kevin Min Seong Park <park.minseong.kevin@gmail.com>
2022-11-09 09:38:14 -0500

    Hyperparameter Notebook

--- DRLPDE_param_solver.py
+++ DRLPDE_param_solver.py
@@ -1,10 +1,23 @@
-############## Walker and Boundary Parameters ############
+############## Parameters for the Solver ############
 
 # Time step
 dt = 1e-4
 
 # exit tolerance
-tol = 1e-6
+tol = 1e-8
+
+# Solver Type
+#    Martingale
+#        dt, tol, num_walkers, num_ghost, update_walkers
+#
+#    AutoDiff
+#        num_points
+#
+#    FiniteDiff
+#        dt, tol, num_points, discretization
+
+# Universal parameters
+#     num_batch, update_points_every
 
 # Number of walkers
 num_walkers = 2**12
@@ -12,12 +25,13 @@ num_ghost = 128
 num_batch = 2**12
 
 # Update walkers
-# Options: 
 #    move -- moves walkers to one of their new locations
 #    remake -- remake walkers at each training step
 #    fixed -- keeps walkers fixed
 update_walkers = 'remake'
 update_walkers_every = 1
+
+# TODO: Bdry and Initial points can be chosen automatically from number of interior points
 
 # Number of boundary points 
 num_bdry = 2**12
@@ -27,7 +41,7 @@ num_batch_bdry = 2**12
 num_init = 2**12
 num_batch_init = 2**12
 
-############## Training Parameters #######################
+############## Deep Learning Parameters #######################
 
 # Training epochs
 num_epoch = 1000
@@ -38,8 +52,8 @@ nn_depth = 60
 nn_width = 4
 
 # Weighting of losses
-lambda_bell = 1e0 # 1e-2/dt
-lambda_bdry = 1e4
+lambda_bell = 1e-2 # 1e-2/dt
+lambda_bdry = 1e2
 lambda_init = 1e-1
 
 # Learning rate
